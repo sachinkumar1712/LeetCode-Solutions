1700027879f4dc2079d753af3c0b5fdc8fa80542
Sachin Kumar <sachinbaghel1712@gmail.com>
2026-07-12 16:49:13 +0530

    Time: 4 ms (59.02%), Space: 124.7 MB (51.3%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        int idx = 1;
+        int fidx = -1;
+        int sidx = -1;
+        ListNode* a = head;
+        ListNode* b = head->next;
+        ListNode* c = head->next->next;
+
+        while(c!=NULL){
+            if(b->val > a->val && b->val > c->val || b->val < a->val && b->val < c->val){
+                if(fidx==-1) fidx = idx;
+                else sidx = idx;
+            }
+            a=a->next;
+            b=b->next;
+            c=c->next;
+            idx++;
+        }
+        if(sidx==-1) return{-1,-1};
+        int maxD = sidx - fidx;
+        int minD = INT_MAX;
+        idx = 1;
+        fidx = -1;
+        sidx = -1;
+        a = head;
+        b = head->next;
+        c = head->next->next;
+        while(c!=NULL){
+            if(b->val > a->val && b->val > c->val || b->val < a->val && b->val < c->val ){
+                fidx = sidx;
+                sidx = idx;
+                if(fidx!=-1){
+                   int d = sidx - fidx;
+                   minD = min(minD,d); 
+                }
+            }
+            a=a->next;
+            b=b->next;
+            c=c->next;
+            idx++;
+        }
+        return{minD,maxD};
+    }
+};
