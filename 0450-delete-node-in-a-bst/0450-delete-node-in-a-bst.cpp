18b37d085c97d634333ead5041073219c4ba652b
Sachin Kumar <sachinbaghel1712@gmail.com>
2026-09-24 23:04:26 +0530

    Time: 0 ms (100%), Space: 34.5 MB (14.76%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderPredecessor(TreeNode* root){
+        TreeNode* pred = root->left;
+        while(pred->right!=NULL){
+            pred = pred->right;
+        }
+        return pred;
+    }
+     TreeNode* inorderSuccessor(TreeNode* root){
+        TreeNode* succ= root->right;
+        while(succ->left!=NULL){
+            succ = succ->left;
+        }
+        return succ;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root == NULL) return NULL;
+        if(root->val == key){
+            //case 1: no child 
+            if(root->left == NULL && root->right == NULL) return NULL;
+            //case 2: 1 child
+            if(root->left == NULL || root->right == NULL){
+                if(root->left!=NULL) return root->left;
+                else return root->right;
+            }
+            //case 3: 2child
+            if(root->left!=NULL && root->right != NULL){
+                //replace with root with its inorder predecessor/successor
+                //after replacing delete the pred/suc
+                TreeNode* pred = inorderPredecessor(root);
+                root->val = pred->val;
+                root->left = deleteNode(root->left ,pred->val);
+            }
+        }
+         //leaf child
+        else if(root->val > key) root->left = deleteNode(root->left ,key);
+        else root->right = deleteNode(root->right,key);
+        return root;
+    }
+};
